acd920c5aa5a77c8d6f47e3c09a729d63e7b6bdb
Luis Weitl <weitlluis@proton.me>
2025-09-26 09:07:54 +0200

    Implemented "Cargorush.toml" creation and checking

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,11 +1,43 @@
 #[cfg(debug_assertions)]
 use crate::debug;
 
-use crate::info;
+use std::{fs::File, io::ErrorKind, process};
+
+use crate::{error, info};
 
 pub fn cli() {
     let current_dir = std::env::current_dir().unwrap_or_default();
+    match File::open("Cargo.toml") {
+        Ok(_) => (),
+        Err(err) => {
+            error!(&format!(
+                "Could not find 'Cargo.toml' in current directory. \
+                    Make sure your working directory is set to your Rust's project root! \
+                    Error: {}",
+                err
+            ));
+            process::exit(0)
+        }
+    };
     #[cfg(debug_assertions)]
-    debug!(&format!("Trying initialization at {:?}", current_dir));
+    debug!(&format!(
+        "Trying initialization at {}",
+        current_dir.display()
+    ));
     info!("Initializing project...");
+    match File::create_new("Cargorush.toml") {
+        Ok(_) => {
+            info!(&format!(
+                "Initialized cargo-rush in {}",
+                current_dir.display()
+            ));
+        }
+        Err(err) => match err.kind() {
+            ErrorKind::AlreadyExists => {
+                error!("cargo-rush has already been initialized in this directory!");
+                process::exit(0)
+            }
+            _ => (),
+        },
+    };
 }
